19cb59aaa374bb36b814f3f7ea96ebfbccce5191
Jonathan De Baere <jonathan.debaere@digitalpulse.be>
2022-11-02 16:35:23 +0100

    search translations

--- src/models/SourceMessage.php
+++ src/models/SourceMessage.php
@@ -86,13 +86,23 @@ class SourceMessage extends ActiveRecord
         if (is_array($filters)) {
             foreach ($filters as $filterKey => $filterValues) {
                 if (!empty($filterValues)) {
+
                     if ($filterKey == 'category') {
 //                    \Craft::dd(['category' =>  $filterValues]);
                         $query->andWhere(['category' =>  $filterValues]);
                     }
+
                     if ($filterKey == 'search') {
-                        $query->andWhere(['like', 'message', $filterValues]);
+                        $query->andWhere(['or',
+                            ['like', 'message', $filterValues],
+                            ['exists', (new Query())
+                                ->from(Message::tableName())
+                                ->where('dp_translations_message.id = dp_translations_source_message.id')
+                                ->andWhere(['like', 'translation', $filterValues])
+                            ]
+                        ]);
                     }
+
                     if ($filterKey == 'missing') {
                         $query->andWhere(['not exists', (new Query())
                             ->from(Message::tableName())
@@ -102,9 +112,11 @@ class SourceMessage extends ActiveRecord
                             ->andWhere('translation <> \'\'')
                         ]);
                     }
+
                     if ($filterKey == 'order') {
                         $query->orderBy($filterValues);
                     }
+
                 }
             }
         }
